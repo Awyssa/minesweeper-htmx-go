6e5cebf71bb517fdc849b092e557b8633f88f303
Awyssa <michael.ehson@gmail.com>
2025-02-22 01:40:49 +0000

    init

---
+++ cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "html/template"
+  "io"
+  "net/http"
+
+  "github.com/labstack/echo/v4"
+  "github.com/labstack/echo/v4/middleware"
+)
+
+type TemplateRenderer struct {
+  templates * template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+    return t.templates.ExecuteTemplate(w, name, data)
+}
